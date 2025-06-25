0e98d5979c3506591ca98b178271cedcffb7206e
Christian Caballari <christianccm17@gmail.com>
2025-06-25 15:01:29 -0600

    Se cambio titulo del menu, y redireccion por default a main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,22 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-   { path: 'main', loadComponent: () => import('./main/main.page').then(m => m.MainPage)},
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+  {
+    path: 'main',
+    loadComponent: () => import('./main/main.page').then((m) => m.MainPage),
+  },
   {
     path: 'crear',
-    loadComponent: () => import('./crear/crear.page').then( m => m.CrearPage)
+    loadComponent: () => import('./crear/crear.page').then((m) => m.CrearPage),
   },
   {
     path: 'editar/:id',
-    loadComponent: () => import('./editar/editar.page').then( m => m.EditarPage)
-  }
+    loadComponent: () =>
+      import('./editar/editar.page').then((m) => m.EditarPage),
+  },
 ];
